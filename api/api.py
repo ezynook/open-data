6048e36c869f85a99ede56720edcb45116ef28e3
pasit.dev <90300159+ezynook@users.noreply.github.com>
2023-06-08 12:45:37 +0700

    Add function get table id in api

--- api/api.py
+++ api/api.py
@@ -16,7 +16,6 @@ USERNAME = main_config["USERNAME"]
 PASSWORD = main_config["PASSWORD"]
 IPADDR = main_config["IPADDR"]
 DB = main_config["DB"]
-TABLE_ID = main_config["TABLE_ID"]
 
 class SDC_API:
     def __init__(self) -> None:
@@ -25,9 +24,16 @@ class SDC_API:
     def Connect(self):
         engine = create_engine(f"postgresql://{USERNAME}:%s@{IPADDR}/{DB}" % quote(f'{PASSWORD}'))
         return engine
+    #ต้อง Generate API Key ที่ WebUI
+    def getID(dataset):
+        r = requests.get(f"http://192.168.10.47/api/3/action/package_show?id={dataset}", 
+                     headers={'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2ODYyMDI0MTgsImp0aSI6IlVLaGFsSWU1eG1xUjZNR2o3bGQ4amVkTnEzeHB1X2QyZ05hS1VUVENVNkJRVTZaM3VsX295MkVicE1FRzBkSnNKNHluZTFFM0lVRUlaTlVPIn0.-2GI8s-h4MZrZ2MeOtkZvqyWyEw54d5m_p7vLboEekY'})
+        data = r.json()
+        result = data['result']['resources'][0]['id']
+        return result
         
     def editDataExplorer(self):
-        #Call API
+        TABLE_ID = getID("test")
         url = "http://demo-cinspire.myddns.me:9400/json/MEA/usage_distict"
         response = requests.get(url, auth=("admin", "Dci@2560"), verify=False)
         data = response.json()
